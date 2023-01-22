f88c338225389d4b8a615d181378236437a728d0
Ikonostas <Alex-jks1@mail.ru>
2023-01-22 22:50:03 +0300

    suka

--- fraim.java
+++ fraim.java
@@ -1,7 +1,9 @@
  public class fraim {
     public static void main(String[] args) {
         int x = 10;
+        int y = 20;
+
         ++x;
-        System.out.println(x);
+        System.out.println(x+y);
     }
 }
